ef1cfc0daa355c10e3071756b0388e5ee5f1f4d7
Rana Usman <usman4243ch@gmail.com>
2025-05-06 16:17:40 -0700

    add profile router

--- src/app.js
+++ src/app.js
@@ -1,31 +1,33 @@
 import express from "express";
 import authRouter from "./routers/auth/authRoutes.js";
-import categoryRouter from "./routers/categories/category.router.js";
-import subCategoriesRouter from "./routers/categories/subcategories.router.js";
-import { authorizeUser } from "./middlewares/auth.middleware.js";
-import clientRouter from "./routers/client/client_job.router.js";
-import bidRouter from "./routers/bid.router.js";
-import { USER_ROLE } from "./config/enums/enums.js";
-import jobRouter from "./routers/jobs/job.router.js";
+import profileRouter from "./routers/auth/profileRoute.js";
+// import categoryRouter from "./routers/categories/category.router.js";
+// import subCategoriesRouter from "./routers/categories/subcategories.router.js";
+// import { authorizeUser } from "./middlewares/auth.middleware.js";
+// import clientRouter from "./routers/client/client_job.router.js";
+// import bidRouter from "./routers/bid.router.js";
+// import { USER_ROLE } from "./config/enums/enums.js";
+// import jobRouter from "./routers/jobs/job.router.js";
 import upload from "./middlewares/upload.file.js";
 // import authRouter from "./routers/auth/auth.router.js";
 const app = express.Router();
 
 app.use("/auth", authRouter);
+app.use("/profile", profileRouter);
 
-app.use("/categories", categoryRouter);
-app.use("/sub-categories", subCategoriesRouter);
+// app.use("/categories", categoryRouter);
+// app.use("/sub-categories", subCategoriesRouter);
 
-app.use(
-  "/protected-route",
-  authorizeUser([USER_ROLE.ADMIN, USER_ROLE.FREELANCER]),
-  (req, res) => {
-    return res.json({ message: "Protected Route" });
-  }
-);
-app.use("/jobs", jobRouter);
-app.use("/client", clientRouter);
-app.use("/bid", bidRouter);
+// app.use(
+//   "/protected-route",
+//   authorizeUser([USER_ROLE.ADMIN, USER_ROLE.FREELANCER]),
+//   (req, res) => {
+//     return res.json({ message: "Protected Route" });
+//   }
+// );
+// app.use("/jobs", jobRouter);
+// app.use("/client", clientRouter);
+// app.use("/bid", bidRouter);
 
 app.post("/image-upload", upload.single("image"), (req, res) => {
   console.log(req.file);
