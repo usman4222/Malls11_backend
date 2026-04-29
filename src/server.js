5f17cef76973c25e5fc4a4a5749afa7848a519ab
Usman4222 <usman4243ch@gmail.com>
2026-04-29 17:11:10 +0500

    refactor CORS configuration and improve server listening syntax

--- src/server.js
+++ src/server.js
@@ -16,17 +16,26 @@ expApp.use(forms.array());
 
 const PORT = process.env.PORT || 3000;
 
-expApp.use(cors({
-  origin: ["http://localhost:5173", "http://145.223.118.108:3000", "http://145.223.118.108"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", 'PATCH', "DELETE"],
-}));
+expApp.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://145.223.118.108:3000",
+      "http://malls11.com:8083",
+      "http://malls11.com",
+      "https://malls11.com",
+      "http://145.223.118.108",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  }),
+);
 
 expApp.use("/", app);
 expApp.use(errorMiddleware);
 
 dbConnection().then(() => {
-  expApp.listen(PORT, '0.0.0.0', () => {
+  expApp.listen(PORT, "0.0.0.0", () => {
     console.log(`Server listening on port ${PORT}`);
   });
 });
